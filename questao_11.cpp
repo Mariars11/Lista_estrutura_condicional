3d0a16296152931b83285a5498bd5ef3a8445f72
Maria Clara <70076859+Mariars11@users.noreply.github.com>
2022-03-24 17:35:16 -0300

    Update questao_11.cpp

--- questao_11.cpp
+++ questao_11.cpp
@@ -1,6 +1,3 @@
-//disciplina: programação estruturada
-//matricula: UC21100014
-//Nome: Maria Clara Rodrigues Silva
 #include <stdio.h>
 #include <locale.h>
 int main(){
@@ -10,7 +7,7 @@ int main(){
 	float media_exercicios, media_aproveitamento, nota1, nota2, nota3;
 	char conceito;
 	
-	printf("Informe o número de identificação: ");
+	printf("Informe o nÃºmero de identificaÃ§Ã£o: ");
 	scanf("%d", &num_identificacao);
 	printf("Informe a nota1: ");
 	scanf("%f", &nota1);
@@ -18,7 +15,7 @@ int main(){
 	scanf("%f", &nota2);
 	printf("Informe a nota3: ");
 	scanf("%f", &nota3);
-	printf("Informe a média dos exercicios: ");
+	printf("Informe a mÃ©dia dos exercicios: ");
 	scanf("%f", &media_exercicios);
 	//Processamento
 	media_aproveitamento = (nota1 + (nota2 * 2) + (nota3 * 3) + media_exercicios) / 7;
@@ -38,16 +35,16 @@ int main(){
 					else if(media_aproveitamento < 4.0){
 						conceito = 'E';
 				}
-	//Saída
+	//SaÃ­da
 	printf("\n===================================\n");
-	printf("	TABELA COM INFORMAÇÕES\n");
+	printf("	TABELA COM INFORMAÃ‡Ã•ES\n");
 	printf("===================================\n");
-	printf("[Número de identificação]: {%d}\n", num_identificacao);
+	printf("[NÃºmero de identificaÃ§Ã£o]: {%d}\n", num_identificacao);
 	printf("[Nota1]: {%.2f}\n", nota1);
 	printf("[Nota2]: {%.2f}\n", nota2);
 	printf("[Nota3]: {%.2f}\n", nota3);
-	printf("[Média dos exercícios]: {%.2f}\n", media_exercicios);
-	printf("[Média de aproveitamento]: {%.2f}", media_aproveitamento);
+	printf("[MÃ©dia dos exercÃ­cios]: {%.2f}\n", media_exercicios);
+	printf("[MÃ©dia de aproveitamento]: {%.2f}", media_aproveitamento);
 	if(conceito == 'A' || conceito == 'B' || conceito == 'C'){
 		printf("\n\t[APROVADO!]");
 	}
