31c0161e3b18e4cdc5458469c5ccde0e872cfaf5
Maria Clara <70076859+Mariars11@users.noreply.github.com>
2022-03-26 09:41:00 -0300

    Update questao_6.cpp

--- questao_6.cpp
+++ questao_6.cpp
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <locale.h>
+
 int main(){
-	setlocale(LC_ALL, "Portuguese");
-	//Entrada
+	setlocale(LC_ALL, "Portuguese"); 
+
 	int num1, num2;
-	bool checar;
 	
-	printf("Informe o número 1: ");
+	printf("Informe o num1: ");
 	scanf("%d", &num1);
-	printf("Informe o número 2: ");
+	printf("Informe o num2: ");
 	scanf("%d", &num2);
 	
-	if(num1 == num2){
-		checar = 1;
+	if(num1 == 1 && num2 == 1){
+		printf("\nAmbos são verdadeiros!");
 	}
-	else checar = 0;
-	//Processamento
-	if(checar == 1){
-		printf("VERDADEIRO!");
-	}
-	else printf("FALSO!");
-	//Saída
+		else if((num1 == 1 && num2 == 0) || (num1 == 0 && num2 == 1)){
+			printf("\nApenas um é verdadeiro!");
+		}
+			else{
+				printf("\nAmbos são falsos!");	
+			} 
+	
 	return 0;
 }
